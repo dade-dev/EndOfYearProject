fd887851c2d9798009928d4e9865a6df9a0b9a49
peppe17182 <alevisentin891@gmail.com>
2025-04-22 09:04:51 +0200

    removed debug prints

--- secretMessanger/src/msg/controller/Controller.java
+++ secretMessanger/src/msg/controller/Controller.java
@@ -66,7 +66,6 @@ public class Controller implements NetworkService.MessageListener, PeerDiscovery
         try {
             byte[] encrypted = model.encrypt(message);
             String base64 = Base64.getEncoder().encodeToString(encrypted);
-            System.out.println(model.getChatName(selectedIp) +" msg: "+base64);
             network.connectToPeer(selectedIp);
             network.sendMessage(selectedIp, base64);
             model.addMessage(selectedIp, "Tu: " + message);
